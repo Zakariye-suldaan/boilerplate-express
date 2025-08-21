dc2dc04c124510ee7c18c3150909a99292dd112b
Zakaria Sultan <rabadaanmahad@gmail.com>
2025-08-21 08:35:06 +0000

    added name route

--- myApp.js
+++ myApp.js
@@ -33,4 +33,14 @@ app.get('/now', (req, res, next) => {
   res.json({ time: req.time });
 });
 
+//Build an echo server, mounted at the route GET /:word/echo. Respond with a JSON object, taking the structure {echo: word}. You can find the word to be repeated at req.params.word. You can test your route from your browser's address bar, visiting some matching routes, e.g. your-app-rootpath/freecodecamp/echo.
+app.get('/:word/echo', (req, res) => {
+  res.json({ echo: req.params.word });
+});
+
+//Build an API endpoint, mounted at GET /name. Respond with a JSON document, taking the structure { name: 'firstname lastname'}. The first and last name parameters should be encoded in a query string e.g. ?first=firstname&last=lastname.
+app.get('/name', (req, res) => {
+  res.json({ name: `${req.query.first} ${req.query.last}` });
+});
+
 module.exports = app;
