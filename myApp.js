2e23d5c8b42b651a383b11236f3964ab9d2fd934
Zakaria Sultan <rabadaanmahad@gmail.com>
2025-08-21 08:13:10 +0000

    added

--- myApp.js
+++ myApp.js
@@ -1,40 +1,28 @@
 let express = require('express');
 let app = express();
-console.log("Hello World");
+require('dotenv').config();
 
+// Root-level logger middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
 
+// Serve static assets
+app.use("/public", express.static(__dirname + "/public"));
 
+// Serve HTML file on root route
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
+// JSON endpoint with env config
+app.get("/json", (req, res) => {
+  let response = { message: "Hello json" };
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    response.message = response.message.toUpperCase();
+  }
+  res.json(response);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
--- server.js
+++ server.js
@@ -21,17 +21,8 @@ if (!process.env.DISABLE_XORIGIN) {
   });
 }
 
-app.use('/public', express.static(__dirname + '/public'));
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
-});
 
-app.get("/json", (req, res) => {
-  res.json({
-    "message": "Hello json"
-  });
-});
 
 
 const port = process.env.PORT || 3000;
