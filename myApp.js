ca79c334b96b1f9f86c53d1beeb75a9e1e4c418a
Zakaria Sultan <rabadaanmahad@gmail.com>
2025-08-21 08:20:28 +0000

    added route now Time

--- myApp.js
+++ myApp.js
@@ -25,4 +25,12 @@ app.get("/json", (req, res) => {
   res.json(response);
 });
 
+// In the route app.get('/now', ...) chain a middleware function and the final handler. In the middleware function you should add the current time to the request object in the req.time key. You can use new Date().toString(). In the handler, respond with a JSON object, taking the structure {time: req.time}.
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.json({ time: req.time });
+});
+
 module.exports = app;
