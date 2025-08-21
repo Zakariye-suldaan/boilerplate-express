0892e34692749bf086941cf0b59b55125d096587
Zakaria Sultan <rabadaanmahad@gmail.com>
2025-08-21 08:52:02 +0000

    added post name

--- myApp.js
+++ myApp.js
@@ -47,5 +47,8 @@ app.get('/name', (req, res) => {
   res.json({ name: `${req.query.first} ${req.query.last}` });
 });
 
+app.post('/name', (req, res) => {
+  res.json({ name: `${req.body.first} ${req.body.last}` });
+});
 
 module.exports = app;
